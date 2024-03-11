81b32680d5eecc34a6fb87875b015a9a42cc810f
ryung <tlsgksfbd@seoultech.ac.kr>
2024-03-11 23:17:00 +0900

    모드변환시 minju 녹화시작 esc 누르면 꺼짐 네모상자 구현 완료

--- test.py
+++ test.py
@@ -1,29 +1,64 @@
-import cv2
+import cv2 as cv
+import numpy as np
+import random
 
-# 웹캠을 사용하기 위해 VideoCapture 객체를 생성합니다.
-cap = cv2.VideoCapture(0)  # 0은 기본 웹캠을 의미합니다.
+# Set the video file format and FourCC code, and the file name
+target_format = "avi"
+target_fourcc = "XVID"
+target_file = "minju_recoder" + "." + target_format
 
-# VideoCapture 객체가 초기화되었는지 확인합니다.
-if not cap.isOpened():
-    print("웹캠을 열 수 없습니다.")
-    exit()
+# Open the video stream
+video = cv.VideoCapture("rtsp://210.99.70.120:1935/live/cctv001.stream")
 
-while True:
-    # 웹캠에서 프레임을 읽어옵니다.
-    ret, frame = cap.read()
+# Set recording state, negative image state, and whether to apply negative image
+isRecord = False
+isNega = False
+isNegaOn = False
 
-    # 프레임을 정상적으로 읽었는지 확인합니다.
-    if not ret:
-        print("프레임을 읽을 수 없습니다.")
-        break
+if video.isOpened() :
+    target = cv.VideoWriter()
+    fps = video.get(cv.CAP_PROP_FPS)
+    if fps > 60 :
+        fps = 60
+    wait_msec = int(1 / fps * 1000)
 
-    # 프레임을 화면에 표시합니다.
-    cv2.imshow('Webcam', frame)
+    while True :
+        # Read frames from the video
+        valid, img = video.read()
+        # Randomly decide whether to apply negative image
+        isNega = random.choice([True, False])
+        if isNega == True :
+            if isNegaOn : 
+                img = 255 - img
+        h, w, *_ = img.shape
+        is_color = (img.ndim > 2) and (img.shape[2] > 1)
+        if isRecord == False : 
+            # Display the video frame
+            cv.imshow("Video Player", img)
+            key = cv.waitKey(wait_msec)
+            if key == ord(" ") :
+                isRecord = True
+                target.open(target_file, cv.VideoWriter_fourcc(*target_fourcc), fps, (w, h), is_color)
+            elif key == 27 :
+                break
+            elif key == ord("n") :
+                isNegaOn = not isNegaOn
 
-    # 'q' 키를 누르면 종료합니다.qwdtrtytsercdwddwd
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        elif isRecord == True:
+            # Add a rectangle to the recording frame
+            cv.rectangle(img, (10, 10), (w - 10, h - 10), (0, 0, 255), thickness=2)
+            
+            target.write(img)
+            cv.imshow("Video Player", img)
+            key = cv.waitKey(wait_msec)
+            if key == ord(" ") :
+                target.release()
+                isRecord = False
+            elif key == 27 :
+                break
+            elif key == ord("n") :
+                isNegaOn = not isNegaOn
+            
+    
+    cv.destroyAllWindows()
 
-# 사용이 끝난 자원을 해제합니다.
-cap.release()
-cv2.destroyAllWindows()
